1ce1d2b1801b320c0cc39ec5a307a9f7dfeac5ac
szjme <szjme@outlook.com>
2026-03-11 13:03:57 +0800

    Update openclaw.sh

--- openclaw.sh
+++ openclaw.sh
@@ -1,1 +1,17 @@
+#
+# https://news.qq.com/rain/a/20260204A06Y2500
+
+# https://www.feishu.cn/content/article/7613711414611463386
+#
+
 curl -fsSL https://openclaw.ai/install.sh | bash
+
+export FS_APP_SEC=https://open.feishu.cn/app/cli_a92421396c79dbd3/baseinfo
+
+openclaw onboard
+
+openclaw gateway run
+
+openclaw status
+
+openclaw gateway restart
